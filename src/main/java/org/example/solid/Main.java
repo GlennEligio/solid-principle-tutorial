bbeee04068a2d9577cef311379d466ce16046ff7
John Glenn Eligio <glenneligio@gmail.com>
2024-05-08 20:59:08 +0800

    Initial setup

---
+++ src/main/java/org/example/solid/Main.java
@@ -0,0 +1,15 @@
+package org.example.solid;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        AreaCalculator areaCalculator = new AreaCalculator();
+        Circle circle = new Circle(6);
+        Square square = new Square(5);
+        List<Object> shapes = List.of(circle, square);
+        int sum = areaCalculator.sum(shapes);
+        System.out.println("Sum of area = " + sum);
+    }
+}
---
+++ src/main/java/org/example/solid/Square.java
@@ -0,0 +1,10 @@
+package org.example.solid;
+
+public class Square {
+
+    private final int length;
+
+    public Square(int length) { this.length = length; }
+
+    public int getLength() { return length; }
+}
